29d076ddc64e875fe7d1575b8d8bf0d62d677128
Bharath B C <bharathbc2017@gmail.com>
2023-07-30 19:52:32 +0530

    First commit

---
+++ src/com/uttara/Test1.java
@@ -0,0 +1,12 @@
+package com.uttara;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String s = "yes";
+		
+		if(s=="yes")
+			System.out.println("then eat..");
+		else
+			System.out.println("do work..");
+	}
+}
